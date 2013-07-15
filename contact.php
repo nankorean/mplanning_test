10b726c8762e713f7245bb607b49dc2c585021a8
nankorean <membridger@me.com>
2013-07-15 22:09:05 +0900

    1.0

---
+++ contact.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = $_REQUEST['email']; 
+$EmailTo = "contact_us@daum.net"; // Your email address here
+$Subject = "Contact form";
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  echo "Error";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= "\n";
+$Body .= "\n";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  echo "E-mail successfully Sent. Thank you! We will contact you ASAP.";
+}
+else{
+  echo "Error";
+}
+?>
+
+<h4><a href="http://www.mplanning.co.kr">mplanning homepage return</a></h4>
